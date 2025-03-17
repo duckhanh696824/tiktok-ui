ceaea85800c65fc3a26d66eb84077cb3402d229d
duc_khanh <nguyenduckhanh12tn1nh2021@gmail.com>
2025-03-17 23:53:59 +0700

    style: Divide the frame for the layout

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,16 +1,20 @@
+import classNames from 'classnames/bind';
 import Header from '~/components/Layout/components/Header';
- import Sidebar from './Sidebar';
- 
- function DefaultLayout({ children }) {
-     return (
-         <div>
-             <Header />
-             <div className="container">
-                 <Sidebar />
-                 <div className="content">{children}</div>
-             </div>
-         </div>
-     );
- }
- 
- export default DefaultLayout;
+import Sidebar from './Sidebar';
+import styles from './DefaultLayout.module.scss';
+
+const cx = classNames.bind(styles);
+
+function DefaultLayout({ children }) {
+  return (
+    <div className={cx('wrapper')}>
+      <Header />
+      <div className="container">
+        <Sidebar />
+        <div className={cx('content')}>{children}</div>
+      </div>
+    </div>
+  );
+}
+
+export default DefaultLayout;
--- src/components/Layout/HeaderOnly/index.js
+++ src/components/Layout/HeaderOnly/index.js
@@ -1,6 +1,6 @@
 import Header from '~/components/Layout/components/Header';
  
- function DefaultLayout({ children }) {
+function HeaderOnly({ children }) {
      return (
          <div>
              <Header />
@@ -11,4 +11,4 @@ import Header from '~/components/Layout/components/Header';
      );
  }
  
- export default DefaultLayout;
+ export default HeaderOnly;
